38e37319a6b49c8c0c2b329a4d1b8c492f1d54f7
Vincent Miller <vmiller0725@gmail.com>
2022-08-14 16:17:25 +0100

    Package installer working.

--- Editor/InfohazardPackageInstaller.cs
+++ Editor/InfohazardPackageInstaller.cs
@@ -21,6 +21,7 @@
 // SOFTWARE.
 
 // Please feel free to copy and modify this file in order to make your own installer!
+// This file was not created with good design in mind, it is just an easy way to get my packages into your project.
 
 using System;
 using System.Collections;
@@ -39,50 +40,56 @@ using Debug = UnityEngine.Debug;
 using PackageInfo = UnityEditor.PackageManager.PackageInfo;
 
 namespace Infohazard.PackageInstaller.Editor {
-    public class InstallablePackage {
+    public class ManagedPackageInfo {
         public string DisplayName { get; set; }
         public string PackageIdentifier { get; set; }
+        public string RepoName { get; set; }
         public string GitUrl { get; set; }
         public string GitZipUrl { get; set; }
         public string Description { get; set; }
         public string[] Dependencies { get; set; }
 
-        public static readonly InstallablePackage[] Packages = {
-            new InstallablePackage {
+        public static readonly ManagedPackageInfo[] Packages = {
+            new ManagedPackageInfo {
                 DisplayName = "Core",
                 PackageIdentifier = "com.infohazard.core",
+                RepoName = "Infohazard.Core",
                 GitUrl = "https://github.com/vcmiller/Infohazard.Core.git",
                 GitZipUrl = "https://api.github.com/repos/vcmiller/Infohazard.Core/zipball",
                 Description = "A collection of useful functionality for Unity, which is used by my other packages.",
                 Dependencies = Array.Empty<string>(),
             },
-            new InstallablePackage {
+            new ManagedPackageInfo {
                 DisplayName = "Sequencing",
                 PackageIdentifier = "com.infohazard.sequencing",
+                RepoName = "Infohazard.Sequencing",
                 GitUrl = "https://github.com/vcmiller/Infohazard.Sequencing.git",
                 GitZipUrl = "https://api.github.com/repos/vcmiller/Infohazard.Sequencing/zipball",
                 Description = "A system for creating startup and level loading sequences, as well as saving game state.",
                 Dependencies = new[]{ "com.infohazard.core" }
             },
-            new InstallablePackage {
+            new ManagedPackageInfo {
                 DisplayName = "State System",
                 PackageIdentifier = "com.infohazard.statesystem",
+                RepoName = "Infohazard.StateSystem",
                 GitUrl = "https://github.com/vcmiller/Infohazard.StateSystem.git",
                 GitZipUrl = "https://api.github.com/repos/vcmiller/Infohazard.StateSystem/zipball",
                 Description = "A system for creating and applying states which override a component's values.",
                 Dependencies = new[] { "com.infohazard.core" }
             },
-            new InstallablePackage {
+            new ManagedPackageInfo {
                 DisplayName = "HyperNav",
                 PackageIdentifier = "com.infohazard.hypernav",
+                RepoName = "Infohazard.HyperNav",
                 GitUrl = "https://github.com/vcmiller/Infohazard.HyperNav.git",
                 GitZipUrl = "https://api.github.com/repos/vcmiller/Infohazard.HyperNav/zipball",
                 Description = "A 3D volume-based navigation system.",
                 Dependencies = new[]{ "com.infohazard.core" }
             },
-            new InstallablePackage {
+            new ManagedPackageInfo {
                 DisplayName = "Slightly Better Rats",
                 PackageIdentifier = "com.infohazard.slightlybetterrats",
+                RepoName = "SlightlyBetterRats",
                 GitUrl = "https://github.com/vcmiller/SlightlyBetterRats.git",
                 GitZipUrl = "https://api.github.com/repos/vcmiller/SlightlyBetterRats/zipball",
                 Description = "Various Unity utilities that are not general enough or not of sufficient code quality to be in their own packages.",
@@ -94,11 +101,11 @@ namespace Infohazard.PackageInstaller.Editor {
             }
         };
 
-        public static InstallablePackage GetPackageInfo(string identifier) =>
+        public static ManagedPackageInfo GetPackageInfo(string identifier) =>
             Packages.FirstOrDefault(pkg => pkg.PackageIdentifier == identifier);
 
         public static void GetPackageDependencies(string identifier, HashSet<string> set) {
-            InstallablePackage pkg = GetPackageInfo(identifier);
+            ManagedPackageInfo pkg = GetPackageInfo(identifier);
             if (pkg == null) {
                 Debug.LogError($"Package not found: {identifier}.");
                 return;
@@ -125,9 +132,12 @@ namespace Infohazard.PackageInstaller.Editor {
         private HashSet<string> _selectedDependencies;
         private bool _dependenciesFoldout;
         private string[] _installOptions;
-        private Dictionary<string, PackageInfo> _installedPackages;
+        private string[] _installOptionsRequired;
+        private Dictionary<string, InstalledManagedPackageInfo> _installedPackages;
 
         private IEnumerator _currentAction;
+
+        private string _gitModulesPath;
 
         [MenuItem("Infohazard/Installer")]
         public static void ShowWindow() {
@@ -147,25 +157,45 @@ namespace Infohazard.PackageInstaller.Editor {
             }
         }
 
-        private static bool CheckGitStatus() {
-            ProcessStartInfo processInfo = new ProcessStartInfo("git", "status");
+        private static bool ExecuteProcess(string command, string args, bool showMessages) {
+            ProcessStartInfo processInfo = new ProcessStartInfo(command, args) {
+                RedirectStandardError = true,
+                UseShellExecute = false,
+            };
             Process process = Process.Start(processInfo);
             if (process != null) {
                 process.WaitForExit();
                 bool success = process.ExitCode == 0;
+                if (!success && showMessages) {
+                    EditorUtility.DisplayDialog("Process returned failure", process.StandardError.ReadToEnd(), "OK");
+                }
                 process.Close();
                 return success;
             } else {
+                if (showMessages) {
+                    EditorUtility.DisplayDialog("Process failed ot start", processInfo.ToString(), "OK");
+                }
                 return false;
             }
         }
 
+        private static bool CheckGitStatus() {
+            return ExecuteProcess("git", "status", false);
+        }
+
         private void OnEnable() {
-            _list = new ReorderableList(InstallablePackage.Packages, typeof(InstallablePackage), false, false, false, false);
+            _list = new ReorderableList(ManagedPackageInfo.Packages, typeof(ManagedPackageInfo), false, false, false, false);
             _list.headerHeight = EditorGUIUtility.singleLineHeight;
             _list.drawElementCallback = DrawElementCallback;
             _list.multiSelect = false;
+            Refresh();
+
+            EditorApplication.update += EditorApplication_Update;
+        }
+
+        private void Refresh() {
             _canUseSubmodule = CheckGitStatus();
+            _gitModulesPath = Path.Combine(Application.dataPath, "..", ".gitmodules");
             
             List<string> options = new List<string> {
                 "Not Installed",
@@ -174,9 +204,9 @@ namespace Infohazard.PackageInstaller.Editor {
             };
             if (_canUseSubmodule) options.Add("Git Submodule");
             _installOptions = options.ToArray();
+            options.RemoveAt(0);
+            _installOptionsRequired = options.ToArray();
             _currentAction = RefreshPackages();
-
-            EditorApplication.update += EditorApplication_Update;
         }
 
         private void OnDisable() {
@@ -228,12 +258,12 @@ namespace Infohazard.PackageInstaller.Editor {
                 EditorGUILayout.HelpBox("Select a package for more info.", MessageType.Info);
             } else {
                 int index = _list.selectedIndices[0];
-                InstallablePackage package = InstallablePackage.Packages[index];
+                ManagedPackageInfo package = ManagedPackageInfo.Packages[index];
 
                 if (_selectedIndex != index || _selectedDependencies == null) {
                     _selectedIndex = index;
                     _selectedDependencies = new HashSet<string>();
-                    InstallablePackage.GetPackageDependencies(package.PackageIdentifier, _selectedDependencies);
+                    ManagedPackageInfo.GetPackageDependencies(package.PackageIdentifier, _selectedDependencies);
                     _selectedDependencies.Remove(package.PackageIdentifier);
                 }
                 
@@ -250,22 +280,32 @@ namespace Infohazard.PackageInstaller.Editor {
         }
 
         private void DrawElementCallback(Rect rect, int index, bool isActive, bool isFocused) {
-            InstallablePackage package = InstallablePackage.Packages[index];
+            ManagedPackageInfo package = ManagedPackageInfo.Packages[index];
             EditorGUI.LabelField(rect, package.DisplayName);
 
             Rect buttonRect = rect;
             buttonRect.xMin = buttonRect.xMax - 150;
             int oldStatus = OptionNone;
-            PackageInfo packageInfo;
-            if (_installedPackages.TryGetValue($"{package.PackageIdentifier}@{package.GitUrl}", out packageInfo)) {
-                oldStatus = OptionGitURL;
-            } else if (_installedPackages.TryGetValue(package.PackageIdentifier, out packageInfo)) {
-                
+            if (_installedPackages.TryGetValue(package.PackageIdentifier, out var packageInfo)) {
+                if (packageInfo.Info.source == PackageSource.Git) {
+                    oldStatus = OptionGitURL;
+                } else if (packageInfo.Info.source == PackageSource.Embedded) {
+                    oldStatus = packageInfo.IsSubmodule ? OptionSubmodule : OptionEmbedded;
+                }
+            }
+
+            bool required = packageInfo?.DependedBy?.Count > 0;
+            int newStatus;
+
+            if (required) {
+                newStatus = EditorGUI.Popup(buttonRect, oldStatus - 1, _installOptionsRequired) + 1;
+            } else {
+                newStatus = EditorGUI.Popup(buttonRect, oldStatus, _installOptions);
             }
             
-            int newStatus = EditorGUI.Popup(buttonRect, oldStatus, _installOptions);
-            if (newStatus == oldStatus) return;
-            _currentAction = ChangePackageStatus(package, packageInfo, oldStatus, newStatus);
+            if (newStatus != oldStatus) {
+                _currentAction = ChangePackageStatus(package, packageInfo?.Info, oldStatus, newStatus, true);
+            }
         }
 
         private void DisplayError(Request request) {
@@ -281,17 +321,80 @@ namespace Infohazard.PackageInstaller.Editor {
             if (request.Status == StatusCode.Failure) {
                 DisplayError(request);
             } else {
-                _installedPackages = request.Result.ToDictionary(pkg => pkg.packageId);
+                _installedPackages = new Dictionary<string, InstalledManagedPackageInfo>();
+                bool checkSubmodules = _canUseSubmodule && File.Exists(_gitModulesPath);
+                foreach (PackageInfo info in request.Result) {
+                    ManagedPackageInfo installable = ManagedPackageInfo.GetPackageInfo(info.name);
+                    if (installable == null) continue;
+                    InstalledManagedPackageInfo pkg = new InstalledManagedPackageInfo();
+                    pkg.Info = info;
+                    pkg.ManagedInfo = installable;
+                    if (checkSubmodules && info.source == PackageSource.Embedded) {
+                        foreach (string line in File.ReadLines(_gitModulesPath)) {
+                            string trim = line.Trim();
+                            if (trim == $"[submodule \"Packages/{installable.RepoName}\"]")
+                            {
+                                pkg.IsSubmodule = true;
+                                break;
+                            }
+                        }
+                    }
+                    _installedPackages[info.name] = pkg;
+                }
+
+                foreach (var pair in _installedPackages) {
+                    foreach (string dependency in pair.Value.ManagedInfo.Dependencies) {
+                        if (!_installedPackages.TryGetValue(dependency, out InstalledManagedPackageInfo info)) {
+                            continue;
+                        }
+
+                        if (info.DependedBy == null) info.DependedBy = new List<string>();
+                        if (!info.DependedBy.Contains(pair.Key)) info.DependedBy.Add(pair.Key);
+                    }
+                }
             }
         }
 
-        private IEnumerator ChangePackageStatus(InstallablePackage package, PackageInfo installed, int oldStatus, int newStatus) {
+        private static void DeleteDirectory(string targetDir) {
+            string[] files = Directory.GetFiles(targetDir);
+            string[] dirs = Directory.GetDirectories(targetDir);
+
+            foreach (string file in files) {
+                File.SetAttributes(file, FileAttributes.Normal);
+                File.Delete(file);
+            }
+
+            foreach (string dir in dirs) {
+                DeleteDirectory(dir);
+            }
+
+            Directory.Delete(targetDir, false);
+        }
+
+        private IEnumerator ChangePackageStatus(ManagedPackageInfo package, PackageInfo installed, int oldStatus, int newStatus, bool refresh) {
             if (oldStatus == OptionGitURL) {
                 RemoveRequest removeRequest = Client.Remove(package.PackageIdentifier);
                 while (!removeRequest.IsCompleted) yield return null;
                 if (removeRequest.Status == StatusCode.Failure) {
                     DisplayError(removeRequest);
                     yield break;
+                }
+            } else if (oldStatus == OptionEmbedded) {
+                Directory.Delete(installed.resolvedPath, true);
+            } else if (oldStatus == OptionSubmodule) {
+                bool result = ExecuteProcess("git", $"rm -f Packages/{package.RepoName}", true);
+                string dir = Path.Combine(Application.dataPath, "..", ".git", "modules", "Packages", package.RepoName);
+                DeleteDirectory(dir);
+                ExecuteProcess("git", $"config --remove-section submodule.Packages/{package.RepoName}", true);
+                if (!result) yield break;
+            }
+
+            if (newStatus != OptionNone) {
+                foreach (string dependency in package.Dependencies) {
+                    if (_installedPackages.ContainsKey(dependency)) continue;
+                    ManagedPackageInfo pkg = ManagedPackageInfo.GetPackageInfo(dependency);
+                    IEnumerator changeStatus = ChangePackageStatus(pkg, null, OptionNone, newStatus, false);
+                    while (changeStatus.MoveNext()) { }
                 }
             }
 
@@ -305,12 +408,40 @@ namespace Infohazard.PackageInstaller.Editor {
                 using UnityWebRequest request = UnityWebRequest.Get(package.GitZipUrl);
                 UnityWebRequestAsyncOperation operation = request.SendWebRequest();
                 while (!operation.isDone) yield return null;
+                if (request.result != UnityWebRequest.Result.Success) {
+                    EditorUtility.DisplayDialog("HTTP Request Failed", request.error, "OK");
+                    yield break;
+                }
                 string tempPath = Path.GetTempFileName();
+                string tempDir = Path.Combine(Path.GetTempPath(), $"Package_{package.PackageIdentifier}");
+                if (Directory.Exists(tempDir)) Directory.Delete(tempDir, true);
+                Directory.CreateDirectory(tempDir);
+                
                 File.WriteAllBytes(tempPath, request.downloadHandler.data);
-                ZipFile.ExtractToDirectory(tempPath, "C:\\Users\\vmill\\Desktop");
+                ZipFile.ExtractToDirectory(tempPath, tempDir);
+
+                string unpackedDir = Directory.EnumerateDirectories(tempDir).First();
+                
+                Directory.Move(unpackedDir, Path.Combine(Application.dataPath, "..", "Packages", package.RepoName));
+                DeleteDirectory(tempDir);
+                File.Delete(tempPath);
+            } else if (newStatus == OptionSubmodule) {
+                bool result = ExecuteProcess("git", $"submodule add {package.GitUrl} Packages/{package.RepoName}",
+                    true);
+                if (!result) yield break;
             }
-            
-            Client.Resolve();
+
+            if (refresh) {
+                Client.Resolve();
+                Refresh();
+            }
+        }
+        
+        private class InstalledManagedPackageInfo {
+            public PackageInfo Info;
+            public ManagedPackageInfo ManagedInfo;
+            public bool IsSubmodule;
+            public List<string> DependedBy;
         }
     }
 }
